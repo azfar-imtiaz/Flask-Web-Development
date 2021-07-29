bc16a7dc52421682ad7a50f4155bcd0b7537eef4
azfar-imtiaz <azfar.imtiaz@live.com>
2021-07-29 17:59:18 +0200

    Minor checks and improvements, add functionality for marking as undone or deleting completed tasks, use message flashing for signup/login success or failure

--- Project1/app.py
+++ Project1/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, send_file, session, g, flash
 from flask.helpers import url_for
 from werkzeug.utils import redirect
+from sqlite3 import IntegrityError
+
 from data_model import DataModel
 
 
@@ -12,18 +14,16 @@ model = DataModel(db_name='../ToDoList.db')
 @app.before_request
 def before_request():
     # NOTE: From my understanding, the session object maintains information throughout a whole session
-    # This can be used to store a user object, such as a class object
+    #   This can be used to store a user object, such as a class object
     # The g object only stores information till a single, and is typically used to store basic information
     #   like username, or user ID
     g.user_id = None
     if 'user_id' in session:
         g.user_id = session['user_id']
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     if 'user_id' in session:
-        # g.user_id = session['user_id']
-        # list_names = model.get_all_list_names(session['user_id'])
         session['current_list_id'] = None
         session['current_list_name'] = None
         list_names = model.get_all_list_names(g.user_id)
@@ -42,7 +42,7 @@ def about():
 def terms_of_use():
     return render_template('terms_of_use.html')
 
-@app.route('/download_terms_of_use')#, methods=['GET'])
+@app.route('/download_terms_of_use')
 def download_terms_of_use():
     return send_file(
         'static/docs/Terms-and-Conditions.docx',
@@ -53,7 +53,7 @@ def download_terms_of_use():
 def privacy():
     return render_template('privacy.html')
 
-@app.route('/download_privacy_policy')#, methods=['GET'])
+@app.route('/download_privacy_policy')
 def download_privacy_policy():
     return send_file(
         'static/docs/Privacy_Policy.docx',
@@ -63,7 +63,6 @@ def download_privacy_policy():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # session.pop('user_email', None)
         session.pop('user_id', None)
         user_email = request.form['user_email']
         password = request.form['password']
@@ -71,14 +70,15 @@ def login():
         if db_password is not None:
             db_password = db_password[0]
             if db_password == password:
-                # session['user_email'] = user_email
                 session['user_id'] = model.get_user_id(user_email)
                 session['username'] = model.get_user_name(user_email)
                 return redirect(url_for('home'))
             else:
-                return render_template('main_structure.html', login_message='Wrong username or password')    
+                flash("Wrong username or password", category='login-error')
+                return render_template('main_structure.html')
         else:
-            return render_template('main_structure.html', login_message='Wrong username or password')
+            flash("Wrong username or password", category='login-error')
+            return render_template('main_structure.html')
     return render_template('main_structure.html')
 
 @app.route('/display_list', methods=['POST'])
@@ -93,7 +93,7 @@ def display_list():
             elif request.form['action'] == 'Delete':
                 return redirect(url_for('delete_list', list_name=list_name))
     except KeyError:
-        flash("Please select a list")
+        flash("Please select a list", category='view-list-error')
         return redirect(url_for('home'))
 
 @app.route('/display_list/<int:list_id>/<string:list_name>')
@@ -108,11 +108,20 @@ def display_list_with_id(list_id, list_name):
     }
     return render_template('view_list.html', list_tasks_dict=list_tasks_dict)
 
-@app.route('/check_off_items', methods=['POST'])
-def check_off_items():
+@app.route('/check_items', methods=['POST'])
+def check_items():
     marked_tasks_ids = request.form.getlist('task_id')
-    if request.form['action'] == 'Complete':
+    if request.form['action'] == 'Check':
         model.update_task_status(marked_tasks_ids, updated_task_status=1)
+    elif request.form['action'] == 'Delete':
+        model.delete_tasks(marked_tasks_ids)
+    return redirect(url_for('display_list_with_id', list_id=session['current_list_id'], list_name=session['current_list_name']))
+
+@app.route('/uncheck_items', methods=['POST'])
+def uncheck_items():
+    marked_tasks_ids = request.form.getlist('task_id')
+    if request.form['action'] == 'Check':
+        model.update_task_status(marked_tasks_ids, updated_task_status=0)
     elif request.form['action'] == 'Delete':
         model.delete_tasks(marked_tasks_ids)
     return redirect(url_for('display_list_with_id', list_id=session['current_list_id'], list_name=session['current_list_name']))
@@ -130,20 +139,19 @@ def sign_up():
     user_email = request.form['user_email']
     username = request.form['username']
     password = request.form['password']
-    model.add_user(user_email, password, username)
-    return render_template('main_structure.html', signup_message='User added successfully!')
+    try:
+        model.add_user(user_email, password, username)
+        flash("User added successfully!", category='success')
+    except IntegrityError:
+        flash("A user already exists with this email", category='failure')
+    return render_template('main_structure.html')
     
-# This might not be needed
-# @app.route('/dashboard', methods=['GET', 'POST'])
-# def dashboard():
-#     return render_template('dashboard.html')
-
 @app.route('/create_new_list', methods=['POST'])
 def create_new_list():
     list_name = request.form['list_name']
     status = model.create_new_list(g.user_id, list_name)
     if status is False:
-        flash("This list already exists!")
+        flash("This list already exists!", category='add-list-error')
     return redirect(url_for('home'))
 
 @app.route('/delete_list/<string:list_name>')
@@ -161,14 +169,21 @@ def edit_list():
     )
     return redirect(url_for('display_list_with_id', list_id=session['current_list_id'], list_name=session['current_list_name']))
 
-# @app.route('/view_list', methods=['GET'])
-# def view_list():
-#     return render_template('view_list.html')
-
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     session.pop('user_id', None)
+    session.pop('username', None)
+    session.pop('current_list_id', None)
+    session.pop('current_list_name', None)
     return redirect(url_for('home'))
+
+@app.route('/list_view_panel', methods=['POST'])
+def list_view_panel():
+    action = request.form['action']
+    if action == 'home':
+        return redirect(url_for('home'))
+    elif action == 'logout':
+        return redirect(url_for('logout'))
 
 
 if __name__ == '__main__':
