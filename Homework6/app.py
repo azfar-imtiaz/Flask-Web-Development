8fee2436dd00c2e04056ae08025253ec25a6be25
azfar-imtiaz <azfar.imtiaz@live.com>
2021-08-04 17:40:12 +0200

    Remove files and changes for gunicorn and gcloud

--- Homework6/app.py
+++ Homework6/app.py
@@ -203,5 +203,5 @@ def list_view_panel():
         return redirect(url_for('logout'))
 
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=7000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=7000, debug=True)
